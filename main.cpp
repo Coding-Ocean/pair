b3d021a84d66c392c56c635f8c09824430b06b57
Coding-Ocean <eijo524@gmail.com>
2024-02-16 12:49:24 +0900

    first

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include<iostream>
-#include<utility>
-#include<tuple>
 #include<vector>
 #include<list>
 using namespace std;
@@ -19,41 +17,6 @@ void show(const C& data) {
 
 int main() 
 {
-#if 1
-	//pair構造体テンプレート
-	pair<string, int> pair1{"黒柳徹子", 90};
-	
-	/*
-	上記の記述で以下のような構造体が用意される
-	struct pair {
-		string first;
-		int second;
-	}pair1{ "黒柳徹子", 90};
-	*/
-
-	{
-		//メンバ first, second
-		cout << pair1.first << ":" << pair1.second << endl;
-	}
-	{
-		// <<のオーバーロードで可能になるpairの表示
-		cout << pair1 << endl;
-	}
-	{
-		//std::tieで紐づけ
-		string name;
-		int age;
-		tie(name, age) = pair1;
-		cout << name << ";" << age << endl;
-	}
-	{
-		//C++17 構造化拘束
-		auto [name, age] = pair1;
-		cout << name << ";" << age << endl;
-	}
-	cout << endl;
-#else
-
 	//pairのvector配列
 	vector<pair<string, int>> vec;
 	vec.emplace_back("AKB", 48);
@@ -67,7 +30,6 @@ int main()
 	list.emplace_back("櫻坂", 46);
 	list.emplace_back("日向坂", 46);
 	show(list);
-#endif
 
 	system("pause>0");
 }
